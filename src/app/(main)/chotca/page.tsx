c2d38de864d649cf5cb0c9e5863e049aaf868307
havanduoc <havanduoc.dev@gmail.com>
2025-07-02 13:38:33 +0700

    feat: Enhance styling of ChotCaPage component for improved responsiveness

--- src/app/(main)/chotca/page.tsx
+++ src/app/(main)/chotca/page.tsx
@@ -7,7 +7,7 @@ const ChotCaPage = () => {
       <section className="container flex flex-col justify-center gap-16 my-24">
 
         {/* Box with border, border-radius, and box-shadow */}
-        <div className="border border-default-500 rounded-xl shadow-lg p-8 bg-white">
+        <div className="sm:border sm:border-default-500 sm:rounded-xl sm:shadow-lg sm:p-8 bg-white">
           <BanGiaoTable />
         </div>
       </section>
